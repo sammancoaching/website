e95366bc1a1644ee90e0dc46169181709f8a5c4b
Emily Bache <emily@bacheconsulting.com>
2025-08-15 14:49:50 +0000

    . t make output less verbose
    Co-authored-by: (isidore) <isidore@users.noreply.github.com>
    Co-authored-by: Jay Bazuzi <jay@bazuzi.com>

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -1,5 +1,5 @@
 @echo off
 call bundle install || exit /b 1
 call bundle exec jekyll build || exit /b 1 
-call pip install -r requirements.txt || exit /b 1  
+call pip install -r requirements.txt --quiet || exit /b 1  
 call python -m unittest discover tests || exit /b 1
