af450850639981eb421534bbcfd5c1c6256407a1
Emily Bache <emily@bacheconsulting.com>
2025-08-15 15:13:58 +0000

    . t convert to verify_json for more stable results
    Co-authored-by: (isidore) <isidore@users.noreply.github.com>
    Co-authored-by: Jay Bazuzi <jay@bazuzi.com>

--- tests/test_generated_search_json.py
+++ tests/test_generated_search_json.py
@@ -1,10 +1,12 @@
 # approval test to verify that the search.json file is generated correctly
 import unittest
 from pathlib import Path
-from approvaltests import verify
+import json
+from approvaltests import verify_as_json
 
 # go in to _site and read search.json and approve it
 class Test(unittest.TestCase):
     def test_search_json(self):
         path = Path('_site/search.json')
-        verify(path.read_text())
+        data = json.loads(path.read_text(encoding='utf-8'))
+        verify_as_json(data)
