868ff438bd411160d09807199e3b28a8678ece06
Emily Bache <emily@bacheconsulting.com>
2025-08-29 14:33:17 +0000

    . e create junit report in XML format
    Co-authored-by: (isidore) <isidore@users.noreply.github.com>
    Co-authored-by: Jay Bazuzi <jay@bazuzi.com>
    Co-authored-by: Phillippe Bourgau <philou@users.noreply.github.com>
    Co-authored-by: Sam Taggart <stagg54@users.noreply.github.com>

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -2,4 +2,5 @@
 call bundle install || exit /b 1
 call bundle exec jekyll build || exit /b 1 
 call pip install -r requirements.txt --quiet || exit /b 1  
-call python -m unittest discover tests || exit /b 1
+if not exist junit-reports mkdir junit-reports
+call python -m xmlrunner discover -s tests -o junit-reports || exit /b 1
